79b879efe0bca538164ec19facf88e8b8003889b
Poylar <doaleks8@gmail.com>
2023-07-03 19:50:57 +0800

    deploy

--- src/scripts/modules/togglePresentationBlock.js
+++ src/scripts/modules/togglePresentationBlock.js
@@ -1,13 +1,17 @@
-const presentationButton = document.querySelector('.js-show-more');
+const presentationButton = document.querySelectorAll('.js-show-more');
 
 if (presentationButton) {
-  presentationButton.addEventListener('click', () => {
-    document.querySelector('.presentation-block__desc').classList.toggle('active');
-    presentationButton.classList.toggle('active');
-    if (document.querySelector('.presentation-block__desc').classList.contains('active')) {
-      presentationButton.innerHTML = 'Weniger';
-    } else {
-      presentationButton.innerHTML = 'Mehr anzeigen';
-    }
+  presentationButton.forEach((el) => {
+    el.addEventListener('click', () => {
+      const desc = el.closest('.presentation-block').querySelector('.presentation-block__desc');
+      desc.classList.toggle('active');
+
+      el.classList.toggle('active');
+      if (desc.classList.contains('active')) {
+        presentationButton.innerHTML = 'Weniger';
+      } else {
+        presentationButton.innerHTML = 'Mehr anzeigen';
+      }
+    });
   });
 }
